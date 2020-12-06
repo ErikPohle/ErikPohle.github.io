c35cd508c7f937d9d5e6a448edcc4c0f72a03ffc
ErikPohle <erikpohle@hotmail.com>
2020-12-06 20:10:16 +0000

    Deploying to gh-pages from  @ 07c17e8e2a7e425ef00b687f790533fb3f3ca782 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4f8d4aee4ae8f182d92b8bfb7d6af440.js"
+  "/portfolio/precache-manifest.0dce0ec4787b6070ce2b0cc9d3408441.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
